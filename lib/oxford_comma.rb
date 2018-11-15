db18748abee5a7a3fc702f8acc00ab274aa792b3
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-15 17:39:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
    array.join(" and ")
   else 
-    array.join(", ")
+    last = array[-1]
+    array.pop
+    array.join(", ") + "and #{last}"
  end 
 end
